b228684928f373bae3a0b770ecb297a218c3b561
vitalii <vilavren@gmail.com>
2023-05-26 04:54:54 +0500

    added: component Htag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Htag } from '@/components'
+
 export default function Home() {
   return (
     <main>
       <div>
         <p>Get started by editing</p>
+        <Htag tag="h1">Тест</Htag>
       </div>
     </main>
   )
