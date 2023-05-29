6dc7f4f10af2d0ed0827e7edf58214fce9bc3d93
vitalii <vilavren@gmail.com>
2023-05-29 19:35:52 +0500

    added: withLayoutComponent

--- layout/Layout/Layout.tsx
+++ layout/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import cn from 'classnames'
 import style from './P.module.css'
 import { LayoutProps } from './Layout.props'
 import { Footer, Header, SideBar } from '..'
+import { FunctionComponent } from 'react'
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
@@ -16,4 +17,16 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
       <Footer />
     </>
   )
+}
+
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>
+) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    )
+  }
 }
