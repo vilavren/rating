d2fa53d66f2ddf5e8fbe5539e17781215c34eae1
vitalii <vilavren@gmail.com>
2023-06-08 21:09:00 +0500

    added: adaptive layout footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -2,7 +2,18 @@ import cn from 'classnames'
 
 import style from './Footer.module.css'
 import { FooterProps } from './Footer.props'
+import { format } from 'date-fns'
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, style.footer)} {...props}>
+      <div>Rating © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  )
 }
