916b80a7680db1111fc56d91e0ccb5d8b25f74b1
vitalii <vilavren@gmail.com>
2023-06-14 02:54:48 +0500

    added: sidebar layout

--- src/layout/SideBar/SideBar.tsx
+++ src/layout/SideBar/SideBar.tsx
@@ -2,13 +2,16 @@ import cn from 'classnames'
 import React from 'react'
 
 import { Menu } from '../Menu/Menu'
+import Logo from '../logo.svg'
 
 import style from './SideBar.module.css'
 import { SideBarProps } from './SideBar.props'
 
-export const SideBar = ({ ...props }: SideBarProps): JSX.Element => {
+export const SideBar = ({ className, ...props }: SideBarProps): JSX.Element => {
   return (
-    <div {...props}>
+    <div className={cn(className, style.sidebar)} {...props}>
+      <Logo className={style.logo} />
+      <div>Поиск</div>
       <Menu />
     </div>
   )
