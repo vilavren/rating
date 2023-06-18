ae47aa04f89d84679e27b4a09da2f72f5a467902
vitalii <vilavren@gmail.com>
2023-06-18 22:27:46 +0500

    added: styles component Product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,6 +1,10 @@
 import cn from 'classnames'
 import React from 'react'
 
+import { Card } from '../Card/Card'
+import { Rating } from '../Rating/Rating'
+import { Tag } from '../Tag/Tag'
+
 import styles from './Product.module.css'
 import { ProductProps } from './Product.props'
 
@@ -9,5 +13,30 @@ export const Product = ({
   className,
   ...props
 }: ProductProps): JSX.Element => {
-  return <div>{product.title}</div>
+  return (
+    <Card className={styles.product}>
+      <div className={styles.logo}>
+        <img
+          src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+          alt={product.title}
+        ></img>
+      </div>
+      <div className={styles.title}>{product.title}</div>
+      <div className={styles.price}>{product.price}</div>
+      <div className={styles.credit}>{product.credit}</div>
+      <div className={styles.rating}>
+        <Rating rating={product.reviewAvg ?? product.initialRating} />
+      </div>
+      <div className={styles.tag}>
+        {product.categories.map((c) => (
+          <Tag key={c} color={'ghost'}>
+            {c}
+          </Tag>
+        ))}
+      </div>
+      <div className={styles.priceTitle}>цена</div>
+      <div className={styles.creditTitle}>кредит</div>
+      <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+    </Card>
+  )
 }
